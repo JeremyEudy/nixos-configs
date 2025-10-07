1a1fc293aacf02825bfd5ccafb8370858bc1b6c0
Jeremy Eudy <jeremyeudy@gmail.com>
2025-10-07 11:10:39 -0400

    Moved networking into separate file and added jq

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
 # imports {{{
   imports = [
       ./hardware-configuration.nix
+      ./networking.nix
       ./zsh.nix
       ./vim.nix
       ./tmux.nix
@@ -29,40 +30,6 @@
     "ntfs"
     "exfat"
   ];
-# }}}
-# networking/ssh {{{
-  networking.hostName = "full-stampede"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  networking.interfaces.enp39s0.ipv4.addresses = [ {
-    address = "192.168.150.20";
-    prefixLength = 24;
-  } ];
-  networking.interfaces.enp39s0.wakeOnLan.enable = true;
-  networking.defaultGateway = "192.168.150.1";
-  networking.nameservers = [ "192.168.150.4" "1.1.1.1" "1.1.0.0" ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = false;
-      X11Forwarding = true;
-    };
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ 7777 ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
 # }}}
 # locale {{{
   # Set your time zone.
@@ -175,6 +142,7 @@
     borgbackup
     gparted
     bunbun
+    jq
     # Network Stuff
     sshfs
     wireguard-tools
